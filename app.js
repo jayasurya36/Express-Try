b7e6cc6dee95a95b8a468a666d663d18f0c2bfe7
jayasurya36 <jjayasurya83@gmail.com>
2023-03-30 20:35:38 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
+var app = express();
+const bodyParser = require('body-parser');
+const routes = require('./routes/post.route');
+
+const cors = require('cors');
+app.use(cors({
+  origin : "*"
+}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+mongoose.connect(process.env.DB_URL).then(() =>{
+  console.log("Connected to database successfully");
+}).catch(err =>{
+  console.log(err.message);
+})
+app.use('/' , routes);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/' , (req , res) =>{
+  res.send("Working")
+})
+app.listen('5002' , ()=>{
+    console.log("Server listening to the port 5002");
+})
+
+module.exports = app;
---
+++ routes/post.route.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const Post = require('../models/post.model');
+const cloudinary = require('../config/cloudinary');
+const multer = require('../config/multer');
+
+router.post('/post', multer.single('PostImage'), async (req, res) => {
+    try {
+        let result = await cloudinary.uploader.upload(req.file.path);
+        let post = new Post({
+            PostImage: result.secure_url,
+            cloudinary_id: result.public_id,
+            author: req.body.author,
+            location: req.body.location,
+            description: req.body.description
+        })
+        let val = await post.save()
+        res.send({ status : "Success", value : val});
+    }catch(err){
+        res.status(500).send(err.message)
+    }
+})
+router.get('/posts' , async(req , res) =>{
+    try{
+        let posts = await Post.find();
+        res.send(posts)
+    }catch(err){
+        res.send(err.message);
+    }
+})
+module.exports = router;
